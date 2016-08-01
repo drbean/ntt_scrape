15a756cd9a1e680eaa8553328c549cb7e8c75f3e
Dr Bean <drbean@localhost.localdomain>
2016-08-01 16:36:28 +0900

    opt_spec:["u=s", "router_user"] , ["p=s", "password"] , ["r=s", "router_page"] , ["d=s", "prompt_element"] , ["m=s", "mydns_user"] , ["w=s", "mydns_password"] , ["i=s", "mydns_page"]. prompt_element_with_NBSP,\x{A0}_not_working

--- lib/NttScrape/Command/adsl.pm
+++ lib/NttScrape/Command/adsl.pm
@@ -14,7 +14,24 @@ use Encode;
 sub abstract { "scrape webcaster for ADSL IP" }
 sub description { "Scraping webcaster ntt-setup web page API to get ADSL IP addrsss" }
 
+sub usage_desc { "db moodle -p PORT,ie,cluster -u USER -d DATABASE - +t TABLE -f FILE -a ACTION" }
+
+sub opt_spec  {
+        return (
+                ["u=s", "router_user"]
+                , ["p=s", "password"]
+                , ["r=s", "router_page"]
+                , ["d=s", "prompt_element"]
+                , ["m=s", "mydns_user"]
+                , ["w=s", "mydns_password"]
+                , ["i=s", "mydns_page"]
+	);
+}
+
+
 sub execute {
+	my ($self, $opt, $args) = @_;
+
 	my $rows = scraper {
 		process 'tr td', "tdlist[]" => scraper {
 			process 'td', text => 'TEXT';
@@ -22,8 +39,8 @@ sub execute {
 	};
 
 	my $ua = LWP::UserAgent->new;
-	my $caster = HTTP::Request->new(GET => 'http://ntt.setup/cgi-bin/main.cgi?mbg_webname=status');
-	$caster->authorization_basic('', '');
+	my $caster = HTTP::Request->new(GET => $opt->{r});
+	$caster->authorization_basic($opt->{u}, $opt->{p});
 
 	my $ntt_response = $rows->scrape( $ua->request( $caster ) );
 
@@ -38,7 +55,7 @@ sub execute {
 	}
 
 	my $dns = HTTP::Request->new(POST =>
-"http://ipv4.mydns.jp/directip.html?MID=''&PWD=''&IPV4ADDR=$address");
+"$opt->{i}?MID=$opt->{m}&PWD=$opt->{w}&IPV4ADDR=$address");
 	my $dns_response = $ua->request( $dns );
 	print $dns_response->as_string;
 

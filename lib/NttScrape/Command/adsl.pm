d96e28d434b99114a926652bd0b7798d003b5748
Dr Bean <drbean@localhost.localdomain>
2016-08-01 14:13:30 +0900

    rows,caster,passwd,ntt_response.substr_1.POST:ipv4.mydns.jp.MID,PWD,IPV4ADDR

--- lib/NttScrape/Command/adsl.pm
+++ lib/NttScrape/Command/adsl.pm
@@ -15,28 +15,33 @@ sub abstract { "scrape webcaster for ADSL IP" }
 sub description { "Scraping webcaster ntt-setup web page API to get ADSL IP addrsss" }
 
 sub execute {
-	my $tables = scraper {
+	my $rows = scraper {
 		process 'tr td', "tdlist[]" => scraper {
 			process 'td', text => 'TEXT';
 		};
 	};
 
 	my $ua = LWP::UserAgent->new;
-	my $req = HTTP::Request->new(GET => 'http://ntt.setup/cgi-bin/main.cgi?mbg_webname=status');
-	$req->authorization_basic('user', '3dogs');
+	my $caster = HTTP::Request->new(GET => 'http://ntt.setup/cgi-bin/main.cgi?mbg_webname=status');
+	$caster->authorization_basic('', '');
 
-	my $res = $tables->scrape( $ua->request( $req ) );
+	my $ntt_response = $rows->scrape( $ua->request( $caster ) );
 
-$DB::single=1;
-
-	my $tdlist = $res->{tdlist};
+	my $tdlist = $ntt_response->{tdlist};
 	my $prompt = " ADSL\x{a0}IP";
+	my $address;
 	for my $n ( 0 .. $#$tdlist) {
 		next unless $tdlist->[$n]->{text} eq $prompt;
-		my $ip = substr $tdlist->[++$n]->{text}, 2;
-		print Encode::encode("utf8", $ip);
+		my $ip = substr $tdlist->[++$n]->{text}, 1;
+		$address = Encode::encode("utf8", $ip);
 		last;
 	}
+
+	my $dns = HTTP::Request->new(POST =>
+"http://ipv4.mydns.jp/directip.html?MID=''&PWD=''&IPV4ADDR=$address");
+	my $dns_response = $ua->request( $dns );
+	print $dns_response->as_string;
+
 }
 
 1;
